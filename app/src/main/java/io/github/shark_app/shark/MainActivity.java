25875f0ce09ae8434d65223a15bfc3029d702f5c
Divay Prakash <divayprakash3@gmail.com>
2016-11-20 17:02:28 +0530

    Add handlers for buttons

--- app/src/main/java/io/github/shark_app/shark/MainActivity.java
+++ app/src/main/java/io/github/shark_app/shark/MainActivity.java
@@ -1,13 +1,33 @@
 package io.github.shark_app.shark;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.shareButton) Button shareButton;
+    @BindView(R.id.scanButton) Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.shareButton)
+    public void share(View view) {
+        Intent intent = new Intent(this, ShareActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.scanButton)
+    public void scan(View view) {
     }
 }

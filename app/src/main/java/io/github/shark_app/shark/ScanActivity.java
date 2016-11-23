40384bdf7ac0c207489e2fbae397580c6913773a
Divay Prakash <divayprakash3@gmail.com>
2016-11-23 13:29:33 +0530

    Add scanActivity

---
+++ app/src/main/java/io/github/shark_app/shark/ScanActivity.java
@@ -0,0 +1,12 @@
+package io.github.shark_app.shark;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

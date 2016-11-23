d9589e2b1cc192581de625ba580e133dc0309851
Divay Prakash <divayprakash3@gmail.com>
2016-11-23 10:15:32 +0530

    Verify email address not already in database

--- app/src/main/java/io/github/shark_app/shark/RegisterActivity.java
+++ app/src/main/java/io/github/shark_app/shark/RegisterActivity.java
@@ -1,10 +1,14 @@
 package io.github.shark_app.shark;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -12,6 +16,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,7 +28,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.util.regex.Pattern;
 
 import butterknife.BindColor;
@@ -47,8 +56,11 @@ public class RegisterActivity extends AppCompatActivity {
     public String userEmail;
     public String publicKeyFilePath;
     public String privateKeyFilePath;
+    private String getDataTaskOutput;
     private Boolean pickedPublicKeyFile = false;
     private Boolean pickedPrivateKeyFile = false;
+    private GetDataTaskRunner getDataTaskRunner;
+    private ProgressDialog progressDialog;
 
     @BindView(R.id.nameField) EditText nameField;
     @BindView(R.id.emailField) EditText emailField;
@@ -84,14 +96,25 @@ public class RegisterActivity extends AppCompatActivity {
     public void registerUser(View view) {
         boolean errorStatus = validateForm(view);
         if (errorStatus) return;
-        setSharedPreferencesData();
-    }
-
-    private void setSharedPreferencesData(){
         String userName = nameField.getText().toString().trim();
         String userEmail = emailField.getText().toString().trim();
         String userPublicKey = getKeyFromFile(publicKeyFilePath);
         String userPrivateKey = getKeyFromFile(privateKeyFilePath);
+        uploadUserPublicData(userName, userEmail, userPublicKey, userPrivateKey);
+        setSharedPreferencesData(userName, userEmail, userPublicKey, userPrivateKey);
+    }
+
+    private void uploadUserPublicData(String userName, String userEmail, String userPublicKey, String userPrivateKey) {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            postDataTaskRunner(userName, userEmail, userPublicKey, userPrivateKey);
+        } else {
+            makeSnackbar(getWindow().getDecorView().getRootView(), "ERROR : Registration failed due to network connection unavailability!");
+        }
+    }
+
+    private void setSharedPreferencesData(String userName, String userEmail, String userPublicKey, String userPrivateKey){
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = settings.edit();
         editor.putBoolean(PREFS_USER_EXISTS_KEY, true);
@@ -257,5 +280,97 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ShareActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void postDataTaskRunner(String userName, String userEmail, String userPublicKey, String userPrivateKey) {
+        getDataTaskRunner = new GetDataTaskRunner();
+        int getDataTaskResult = 4;
+        try {
+            getDataTaskResult = getDataTaskRunner.execute(userEmail).get();
+        }
+        catch (Exception e) {}
+        boolean proceed = false;
+        switch (getDataTaskResult) {
+            case 0: {
+                if (!getDataTaskOutput.equals("")) {
+                        String error = new String("Email address already in-use!");
+                        makeSnackbar(getWindow().getDecorView().getRootView(), error);
+                        emailField.setError("Please use a different email address");
+                }
+                else {
+                    makeSnackbar(getWindow().getDecorView().getRootView(), "Verified! Proceeding with registration");
+                    proceed = true;
+                }
+                break;
+            }
+            case 1: makeSnackbar(getWindow().getDecorView().getRootView(), "ERROR : Connection timed out!");
+                    break;
+            case 2: makeSnackbar(getWindow().getDecorView().getRootView(), "ERROR : Unable to retrieve data!");
+                    break;
+            case 3: makeSnackbar(getWindow().getDecorView().getRootView(), "ERROR : Unable to retrieve data!");
+                    break;
+            case 4: makeSnackbar(getWindow().getDecorView().getRootView(), "ERROR!");
+                    break;
+        }
+    }
+
+    private class GetDataTaskRunner extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(RegisterActivity.this, ProgressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("Verifying user");
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+        }
+        @Override
+        protected Integer doInBackground(String...params) {
+            String userEmail = params[0];
+            InputStream inputStream = null;
+            try {
+                String getUrl = "http://192.168.55.157:3000/email/userDetails" + "/" + userEmail;
+                URL url = new URL(getUrl);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setConnectTimeout(7000);
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                Log.d("GetData", "Trying to connect");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                int response = httpURLConnection.getResponseCode();
+                Log.d("GetData", "The response is: " + response);
+                inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line, buffer = new String();
+                while ((line = bufferedReader.readLine()) != null) {
+                    buffer += line;
+                    buffer += "\n";
+                }
+                httpURLConnection.disconnect();
+                bufferedReader.close();
+                getDataTaskOutput = buffer;
+                return 0;
+            } catch (SocketTimeoutException e) {
+                return 1;
+            } catch (IOException e) {
+                return 2;
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    }
+                    catch (IOException e) {
+                        return 3;
+                    }
+                }
+            }
+        }
+        @Override
+        protected void onPostExecute(Integer result) {
+            progressDialog.dismiss();
+        }
     }
 }

e0373552d662eabef9a91d2738a65071b87fd3c5
Divay Prakash <divayprakash3@gmail.com>
2016-11-30 07:49:24 +0530

    Add opensourcelicenses activity web view and assets

--- app/src/main/java/io/github/shark_app/shark/OpenSourceLicensesActivity.java
+++ app/src/main/java/io/github/shark_app/shark/OpenSourceLicensesActivity.java
@@ -1,7 +1,8 @@
 package io.github.shark_app.shark;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class OpenSourceLicensesActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class OpenSourceLicensesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_source_licenses);
+        WebView view = (WebView) findViewById(R.id.webview);
+        view.loadUrl("file:///android_asset/open_source_licenses.html");
     }
 }

b2ff3c47a05922acf1e7857c92f297a05db9afb8
Divay Prakash <divayprakash3@gmail.com>
2016-11-30 06:39:50 +0530

    PUT signed key to server

--- app/src/main/java/io/github/shark_app/shark/SignActivity.java
+++ app/src/main/java/io/github/shark_app/shark/SignActivity.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.spongycastle.bcpg.ArmoredOutputStream;
 import org.spongycastle.openpgp.PGPException;
@@ -29,12 +30,16 @@ import org.spongycastle.openpgp.PGPSecretKey;
 import org.spongycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
@@ -63,6 +68,8 @@ public class SignActivity extends AppCompatActivity {
     private String scannedUserEmail;
     private String scannedUserPublicKey;
     private String currentUserPrivateKey;
+    private String putDataTaskOutput;
+    private String scannedUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,7 +149,159 @@ public class SignActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         if (signingDone == true) {
-            makeAlertDialog("Success!", "The key signing procedure has been completed. The signed key of user " + scannedUserName + " will now be uploaded to our servers.");
+            File sdcard = Environment.getExternalStorageDirectory();
+            File directory = new File(sdcard.getAbsolutePath() + "/Signed_Keys/");
+            String filename = scannedUserName + ".asc";
+            File file = new File(directory, filename);
+            String path = file.getAbsolutePath();
+            final String scannedUserSignedPublicKey = getKeyFromFile(path);
+            AlertDialog.Builder builder = new AlertDialog.Builder(SignActivity.this)
+                    .setTitle("Success!")
+                    .setMessage("The key signing procedure has been completed. Press OK to upload the signed key of user " + scannedUserName + " to our servers.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            putScannedUserPublicData(scannedUserSignedPublicKey);
+                        }
+                    });
+            builder.show();
+        }
+    }
+
+    private String getKeyFromFile(String path){
+        String line;
+        String data = "";
+        try {
+            File file = new File(path);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            while ((line = bufferedReader.readLine()) != null) {
+                data += line + "\n";
+            }
+            bufferedReader.close();
+        }
+        catch (IOException e) {e.printStackTrace();}
+        return data;
+    }
+
+    private void putScannedUserPublicData(String scannedUserSignedPublicKey) {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            runPutDataTask(scannedUserSignedPublicKey);
+        } else {
+            makeAlertDialog("ERROR", "Data transmission to server failed due to network connection unavailability");
+        }
+    }
+
+    private void runPutDataTask(String scannedUserSignedPublicKey) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", scannedUserName);
+            jsonObject.put("email", scannedUserEmail);
+            jsonObject.put("publickey", scannedUserSignedPublicKey);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        PutDataTaskRunner postAgainDataTaskRunner = new PutDataTaskRunner();
+        postAgainDataTaskRunner.execute(String.valueOf(jsonObject));
+    }
+
+    private class PutDataTaskRunner extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected void onPreExecute() {
+            progressDialog = new ProgressDialog(SignActivity.this, ProgressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("Posting data to server");
+            progressDialog.setIndeterminate(true);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+            super.onPreExecute();
+        }
+        @Override
+        protected Integer doInBackground(String...params) {
+            String jsonData = params[0];
+            InputStream inputStream = null;
+            try {
+                String postUrl = "http://192.168.55.157:3000/userDetails/" + scannedUserId;
+                URL url = new URL(postUrl);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setConnectTimeout(7000);
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setRequestMethod("PUT");
+                httpURLConnection.setRequestProperty("Content-type", "application/json");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                Writer writer = new BufferedWriter(new OutputStreamWriter(httpURLConnection.getOutputStream(), "UTF-8"));
+                writer.write(jsonData);
+                writer.close();
+                inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                String line;
+                String buffer = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    buffer += line;
+                    buffer += "\n";
+                }
+                httpURLConnection.disconnect();
+                bufferedReader.close();
+                putDataTaskOutput = buffer;
+                System.out.println(buffer);
+                return 0;
+            } catch (SocketTimeoutException e) {
+                return 1;
+            } catch (IOException e) {
+                return 2;
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    }
+                    catch (IOException e) {
+                        return 3;
+                    }
+                }
+            }
+        }
+        @Override
+        protected void onPostExecute(Integer result) {
+            progressDialog.dismiss();
+            putDataTaskComplete(result);
+        }
+    }
+
+    private void putDataTaskComplete(int postDataTaskResult){
+        switch (postDataTaskResult) {
+            case 0: {
+                if (putDataTaskOutput.equals("")) {
+                    makeAlertDialog("Error", "No data has been posted.");
+                }
+                else {
+                    final Intent intent = new Intent(this, MainActivity.class);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignActivity.this)
+                            .setTitle("Success!")
+                            .setMessage("User " + scannedUserName + "\'s data has been sent to server. Press OK to continue.")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                    builder.show();
+                }
+                break;
+            }
+            case 1: makeAlertDialog("Error", "Connection timed out!");
+                break;
+            case 2: makeAlertDialog("Error", "Unable to retrieve data!");
+                break;
+            case 3: makeAlertDialog("Error", "Unable to retrieve data!");
+                break;
+            default: makeAlertDialog("ERROR", "");
+                break;
         }
     }
 
@@ -237,6 +396,7 @@ public class SignActivity extends AppCompatActivity {
                     scannedUserEmail = jsonObject.getString("email");
                     scannedUserName = jsonObject.getString("name");
                     scannedUserPublicKey = jsonObject.getString("publickey");
+                    scannedUserId = jsonObject.getString("_id");
                     scannedEmailField.setText(scannedUserEmail);
                     scannedNameField.setText(scannedUserName);
                     PGPPublicKey pgpPublicKey = PGPClass.getPublicKeyFromString(scannedUserPublicKey);
